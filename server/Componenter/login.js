9a841b465ce1638f5d614aea2e24dd1ca0a1aeaf
Christian Hammervig <christian@hammervig.dk>
2023-07-06 15:09:11 +0200

    server opdateret med log ind og opret bruger

--- server/Componenter/login.js
+++ server/Componenter/login.js
@@ -1,35 +1,38 @@
-const handleSignin = (req, res, knexDb, bcrypt, jwt, dotenv) => {
+const bugerlogind = (req, res, knexDb, bcrypt, jwt, dotenv) => {
     dotenv.config();
-    const { email, password } = req.body;
-    if (!email || !password) {
+    const { brugernavn, password } = req.body;
+    if (!brugernavn || !password) {
         return res.status(400).json('incorrect form submission');
     }
 
     const loadUser = () => {
-        return knexDb.select('id', 'name', 'email').from('kunde')
-            .where('email', '=', email)
+        return knexDb.select('id', 'navn', 'brugernavn', 'email', 'adresse', 'postnr', 'mobil',).from('kunde')
+            .where('brugernavn', '=', brugernavn)
             .then(user => {
                 const id = user[0].id;
-                const name = user[0].name;
+                const navn = user[0].navn;
                 const email = user[0].email;
-                const token = jwt.sign({ role: role, id: id }, process.env.SECRET_OR_KEY);
-                const payload = { role: role, auth: true, token: token, id: id, name: name, email: email, birthdate: birthdate, gender: gender, last_login: last_login, language: language, country: country };
+                const adresse = user[0].adresse;
+                const postnr = user[0].postnr;
+                const mobil = user[0].mobil;
+                const token = jwt.sign({ id: id }, process.env.SECRET_OR_KEY);
+                const payload = { auth: true, token: token, id, navn, email, adresse, postnr, mobil };
                 res.status(200).send(payload);
             })
             .catch(err => res.status(400).json('unable to get user ' + err))
     }
 
-    knexDb.select('email', 'userpassword').from('kunde')
-        .where('email', '=', email)
+    knexDb.select('brugernavn', 'userpassword').from('kunde')
+        .where('brugernavn', '=', brugernavn)
         .then(data => {
-            data[0] === undefined ? res.status(400).send('email') : (
+            data[0] === undefined ? res.status(400).send('brugernavnet findes ikke') : (
                 bcrypt.compareSync(password, data[0].userpassword) === true ?
-                    loadUser() : res.status(400).send('password'))
+                    loadUser() : res.status(400).send('forkert password'))
         })
         .catch(err => res.status(400).json(err));
 }
 
 
 module.exports = {
-    handleSignin
+    bugerlogind
 };
